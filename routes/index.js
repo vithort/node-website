b56315deec1878eaebddcfc8684ba387f813d51b
Vithor <vithort@gmail.com>
2019-11-30 23:45:09 -0300

    Update
    Update

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { page: 'Home', menuId: 'home', message: null });
+    res.render('login', { page: 'Login', menuId: 'login', message: null });
 });
 
-/*router.get('/about', function (req, res, next) {
+router.get('/index', function (req, res, next) {
+    res.render('index', { page: 'Home', menuId: 'index' });
+});
+
+router.get('/about', function (req, res, next) {
     res.render('about', { page: 'About Us', menuId: 'about' });
 });
 
 router.get('/contact', function (req, res, next) {
     res.render('contact', { page: 'Contact Us', menuId: 'contact' });
 });
-*/
 
 router.post('/', function (req, res, next) {
-    console.log(req.body.login)
-    console.log(req.body.password)
-    if ((req.body.login === 'email@teste.com') && (req.body.password === '123456')) {
+    //console.log(req.body.usernameField)
+    //console.log(req.body.passwordField)
+    if ((req.body.usernameField === 'email@teste.com') && (req.body.passwordField === '123456')) {
         console.log('login ok')
         res.render('about', { page: 'About', menuId: 'about', message: null });
     } else {
